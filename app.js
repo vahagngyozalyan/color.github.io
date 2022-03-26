242a4c4e94f29ab1919aa14eb125c9294fda9d54
Vahagn <94637841+vahagngyozalyan@users.noreply.github.com>
2022-03-26 14:28:06 +0400

    Added new files

---
+++ app.js
@@ -0,0 +1,54 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
+document.querySelector("#signup-submit").onclick() = function(event) {
+  event.preventDefault();
+  console.log('work');
+  let name = document.querySelector('#signup-name');
+  let email = document.querySelector('#signup-email');
+  let password = document.querySelectorAll('#signup-pass');
+}
+
+let data = {
+  "name" : name,
+  "pass" : pass,
+  "email" : email,
+}
